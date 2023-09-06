c3f474c7a87a3fe8d1f808e6cf95f5e6f3c46ea2
syafiqah-grabtaxi <syafiqah.raihan@grabtaxi.com>
2023-09-06 18:29:27 +0800

    modify method parameters to be consistent with robfig

--- dcron_test.go
+++ dcron_test.go
@@ -52,7 +52,7 @@ func runNode(t *testing.T, wg *sync.WaitGroup) {
 	dcron := dcron.NewDcron("server1", drv)
 	// 添加多个任务 启动多个节点时 任务会均匀分配给各个节点
 
-	err := dcron.AddFunc("s1 test1", "* * * * *", func() {
+	_, err := dcron.AddFunc("s1 test1", "* * * * *", func() {
 		// 同时启动3个节点 但是一个 job 同一时间只会执行一次 通过 map 判重
 		key := "s1 test1 : " + time.Now().Format("15:04")
 		if _, ok := testData[key]; ok {
@@ -63,7 +63,7 @@ func runNode(t *testing.T, wg *sync.WaitGroup) {
 	if err != nil {
 		t.Error("add func error")
 	}
-	err = dcron.AddFunc("s1 test2", "* * * * *", func() {
+	_, err = dcron.AddFunc("s1 test2", "* * * * *", func() {
 		t.Log("执行 service1 test2 任务", time.Now().Format("15:04:05"))
 	})
 	if err != nil {
@@ -71,12 +71,12 @@ func runNode(t *testing.T, wg *sync.WaitGroup) {
 	}
 
 	testJob := TestJob1{"addtestjob"}
-	err = dcron.AddJob("addtestjob1", "* * * * *", testJob)
+	_, err = dcron.AddJob("addtestjob1", "* * * * *", testJob)
 	if err != nil {
 		t.Error("add func error")
 	}
 
-	err = dcron.AddFunc("s1 test3", "* * * * *", func() {
+	_, err = dcron.AddFunc("s1 test3", "* * * * *", func() {
 		t.Log("执行 service1 test3 任务", time.Now().Format("15:04:05"))
 	})
 	if err != nil {
@@ -97,7 +97,7 @@ func Test_SecondsJob(t *testing.T) {
 	})
 	drv := driver.NewRedisDriver(redisCli)
 	dcr := dcron.NewDcronWithOption(t.Name(), drv, dcron.CronOptionSeconds())
-	err := dcr.AddFunc("job1", "*/5 * * * * *", func() {
+	_, err := dcr.AddFunc("job1", "*/5 * * * * *", func() {
 		t.Log(time.Now())
 	})
 	if err != nil {
@@ -123,15 +123,15 @@ func runSecondNode(id string, wg *sync.WaitGroup, runningTime time.Duration, t *
 		)),
 	)
 	var err error
-	err = dcr.AddFunc("job1", "*/5 * * * * *", func() {
+	_, err = dcr.AddFunc("job1", "*/5 * * * * *", func() {
 		t.Log(time.Now())
 	})
 	require.Nil(t, err)
-	err = dcr.AddFunc("job2", "*/8 * * * * *", func() {
+	_, err = dcr.AddFunc("job2", "*/8 * * * * *", func() {
 		panic("test panic")
 	})
 	require.Nil(t, err)
-	err = dcr.AddFunc("job3", "*/2 * * * * *", func() {
+	_, err = dcr.AddFunc("job3", "*/2 * * * * *", func() {
 		t.Log("job3:", time.Now())
 	})
 	require.Nil(t, err)
